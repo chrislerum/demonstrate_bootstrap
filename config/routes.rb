ad592059785a2aad6cc46de98f2ec53ee95cdbc3
Chris Lerum <chris.lerum@gmail.com>
2015-01-07 10:16:56 -0500

    adds get index bottles route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'bottles#index'
+  get 'bottles' => 'bottles#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
