06674243e9eb4587247ee77371a653938a4395b6
Chris Lerum <chris.lerum@gmail.com>
2015-01-09 11:49:57 -0500

    adds some seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SEEDS!'
+bottles = Bottle.create!([
+  {name: 'Pink Bottle', color: 'Pink'},
+  {name: 'Blue Bottle', color: 'Blue'},
+  {name: 'Red Bottle', color: 'Red'}
+])
