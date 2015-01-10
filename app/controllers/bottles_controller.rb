034c5c06e5ec62d9458d4983f55d9905fc147018
Chris Lerum <chris.lerum@gmail.com>
2015-01-10 00:12:55 -0500

    shows bottle detail on show view

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,4 +2,7 @@ class BottlesController < ApplicationController
   def index
     @bottles = Bottle.all
   end
+  def show
+    @bottle = Bottle.find(params[:id])
+  end
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,24 +1,21 @@
 require 'rails_helper'
 
 feature "Showing bottles" do
-  before do
-    @bottle = FactoryGirl.create(:bottle, name: 'Fred', color: 'Blue')
-  end
-
   scenario "Show several bottles on home page" do
-    @bottle2 = FactoryGirl.create(:bottle, name: 'Georgie', color: 'Red')
+    bottle1 = FactoryGirl.create(:bottle, name: 'Fred', color: 'Blue')
+    bottle2 = FactoryGirl.create(:bottle, name: 'Georgie', color: 'Red')
     visit root_path
-    expect(page).to have_content @bottle2.name
-    expect(page).to have_content "Color: #{@bottle2.color}"
+    expect(page).to have_content bottle1.name
+    expect(page).to have_content "Color: #{bottle1.color}"
+    expect(page).to have_content bottle2.name
+    expect(page).to have_content "Color: #{bottle2.color}"
   end
 
   scenario "When user clicks a bottle, show bottle detail page" do
+    bottle = FactoryGirl.create(:bottle, name: 'Fred', color: 'Blue')
     visit root_path
-    click_link(@bottle.name.downcase)
-  end
-
-  after do
-    expect(page).to have_content @bottle.name
-    expect(page).to have_content "Color: #{@bottle.color}"
+    click_link(bottle.name.downcase)
+    expect(page).to have_content bottle.name
+    expect(page).to have_content "Color: #{bottle.color}"
   end
 end
