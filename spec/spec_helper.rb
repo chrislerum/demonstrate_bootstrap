f2415a33b3789348c7df12fe6db612c65ff96380
Chris Lerum <chris.lerum@gmail.com>
2015-01-16 13:15:51 -0500

    adds database_cleaner, seems we need it after all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,26 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+  config.include Devise::TestHelpers, type: :controller
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 def pageme
