1639e500670ee98cd2f50af6dfc58d9e5dcc18b0
Chris Lerum <chris.lerum@gmail.com>
2015-01-16 13:17:51 -0500

    adds name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113050241) do
+ActiveRecord::Schema.define(version: 20150116034416) do
 
   create_table "bottles", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20150113050241) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
