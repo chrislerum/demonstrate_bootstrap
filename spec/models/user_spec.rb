34a4aa1bd72ca37405b1e91ddfe37749967d48d9
Chris Lerum <chris.lerum@gmail.com>
2015-01-18 11:54:32 -0500

    adds dob to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ describe User do
   it {should validate_presence_of(:first_name)}
   it {should validate_presence_of(:last_name)}
   it {should respond_to :full_name}
+  it {should respond_to :dob}
 end
 
