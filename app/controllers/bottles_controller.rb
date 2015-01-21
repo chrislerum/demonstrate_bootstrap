4846567958429d1e66f8c49a5615a3fc46fab62d
Chris Lerum <chris.lerum@gmail.com>
2015-01-21 09:54:58 -0500

    forces sign up/sign in to see detail of bottle

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,7 +1,10 @@
 class BottlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @bottles = Bottle.all
   end
+
   def show
     @bottle = Bottle.find(params[:id])
     Bottle.do_the_work(3, 5)
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -11,11 +11,12 @@ feature "Showing bottles" do
     expect(page).to have_content "Color: #{bottle2.color}"
   end
 
-  scenario "When user clicks a bottle, show bottle detail page" do
+  scenario "When user clicks a bottle, ask them to sign in" do
     bottle = FactoryGirl.create(:bottle, name: 'Fred', color: 'Blue')
     visit root_path
     click_link(bottle.name.downcase)
-    expect(page).to have_content bottle.name
-    expect(page).to have_content "Color: #{bottle.color}"
+    expect(page).to have_no_content bottle.name
+    expect(page).to have_no_content "Color: #{bottle.color}"
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
