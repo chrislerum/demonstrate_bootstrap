1b8becfe2ed48fd41f5c0a3da7fdc0b00a107adb
Chris Lerum <chris.lerum@gmail.com>
2015-01-21 15:45:35 -0500

    allow clients to see detail page

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -1,5 +1,5 @@
 class BottlesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  #before_action :authenticate_user!, except: [:index]
 
   def index
     @bottles = Bottle.all
