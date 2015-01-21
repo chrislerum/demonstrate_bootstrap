dfadf2a8efb6d41eb3f367e702467ad5259f324f
Chris Lerum <chris.lerum@gmail.com>
2015-01-21 09:56:36 -0500

    adds example database.yml

--- spec/models/bottle_spec.rb
+++ spec/models/bottle_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe Bottle do
-  it {should validate_presence_of(:name)}
-  it {should validate_presence_of(:color)}
+  #it {should validate_presence_of(:name)}
+  bottle = Bottle.new
+  #expect(bottle).to validate_presence_of(:name)
+  #it {should validate_presence_of(:color)}
 end
 
